34b058333f9cffde31204596db9b8eaa3f7510ed
chrisguttandin <chrisguttandin@media-codings.com>
2017-12-01 23:00:41 +0100

    use fast-unique-numbers to generate ids

--- src/module.ts
+++ src/module.ts
@@ -1,16 +1,5 @@
+import { addUniqueNumber } from 'fast-unique-numbers';
 import { IMidiFile, IParseRequest, IParseResponse, IWorkerEvent } from 'midi-json-parser-worker';
-
-const MAX_SAFE_INTEGER = Number.MAX_SAFE_INTEGER || Math.pow(2, 53) - 1;
-
-const generateUniqueId = (set: Set<number>) => {
-    let id = Math.round(Math.random() * MAX_SAFE_INTEGER);
-
-    while (set.has(id)) {
-        id = Math.round(Math.random() * MAX_SAFE_INTEGER);
-    }
-
-    return id;
-};
 
 export { IMidiFile };
 
@@ -21,7 +10,7 @@ export const load = (url: string) => {
 
     const parseArrayBuffer = (arrayBuffer: ArrayBuffer): Promise<IMidiFile> => {
         return new Promise((resolve, reject) => {
-            const id = generateUniqueId(ongoingRecordingRequests);
+            const id = addUniqueNumber(ongoingRecordingRequests);
 
             const onMessage = ({ data }: IWorkerEvent) => {
                 if (data.id === id) {
